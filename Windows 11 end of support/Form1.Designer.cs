9aca4abd6aeafe968c0aed15e5ea047a4fca95a0
sakuzyokun <sakuzyokun.sakuzyo0606@gmail.com>
2025-12-15 00:17:02 +0900

    Add files via upload

---
+++ Windows 11 end of support/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Windows_11_end_of_support
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 33);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(66, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Windows 11 のサポートは、20XX 年 XX 月 XX 日に終了します。詳細について\r\nは、こちらをクリックしてください。\r\n";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(68, 59);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(181, 16);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "今後、このメッセージを表示しない";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(80, 33);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(30, 12);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "こちら";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 116);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Windows 11 end of support/Form1.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_11_end_of_support
+{
+    public partial class Form1 : Form
+    {
+        const string REG_PATH = @"Software\Microsoft\Windows\CurrentVersion\Run";
+        const string REG_NAME = "Win11SupportNotice";
+        public Form1()
+        {
+            InitializeComponent();
+
+            string productName;
+
+            using (RegistryKey key =
+                Registry.LocalMachine.OpenSubKey(
+                    @"SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
+            {
+                productName = key.GetValue("ProductName")?.ToString();
+            }
+
+            if (productName.Contains("Windows Vista"))
+            {
+                label1.Text =
+                    $"Windows Vista のサポートは、2012 年 4 月 10 日に終了します。詳細について\r\n" +
+                    "は、こちらをクリックしてください。";
+            }
+            else if (productName.Contains("Windows 7"))
+            {
+                label1.Text =
+                    $"Windows 7 のサポートは、2014 年 1 月 14 日に終了します。詳細について\r\n" +
+                    "は、こちらをクリックしてください。";
+            }
+            else if (productName.Contains("Windows 8"))
+            {
+                label1.Text =
+                    $"Windows 8 のサポートは、2016 年 1 月 13 日に終了します。詳細について\r\n" +
+                    "は、こちらをクリックしてください。";
+            }
+            else if (productName.Contains("Windows 8.1"))
+            {
+                label1.Text =
+                    $"Windows 8.1 のサポートは、2018 年 1 月 10 日に終了します。詳細について\r\n" +
+                    "は、こちらをクリックしてください。";
+            }
+            /*else if (productName.Contains("Windows 10"))
+            {
+                label1.Text =
+                    $"Windows 10 のサポートは、2025 年 10 月 14 日に終了します。詳細について\r\n" +
+                    "は、こちらをクリックしてください。";
+            }*/
+            else
+            {
+                DateTime now = DateTime.Now;
+
+                // 今月の最終日を取得（30 or 31 / 2月なら28 or 29）
+                int lastDay = DateTime.DaysInMonth(now.Year, now.Month);
+
+                string dateText =
+                    $"{now.Year} 年 {now.Month} 月 {lastDay} 日";
+
+                label1.Text =
+                    $"Windows 11 のサポートは、{now.Year} 年 {now.Month} 月 {lastDay} 日に終了します。詳細について\r\n" +
+                    "は、こちらをクリックしてください。";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (RegistryKey rk = Registry.CurrentUser.OpenSubKey(REG_PATH, false))
+            {
+                if (rk == null || rk.GetValue(REG_NAME) == null)
+                {
+                    // レジストリが無い → False（＝自動起動ON）
+                    checkBox1.Checked = false;
+                    EnableStartup();
+                }
+                else
+                {
+                    // レジストリがある → 自動起動ON状態
+                    checkBox1.Checked = false;
+                }
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                // True → 自動起動OFF
+                DisableStartup();
+            }
+            else
+            {
+                // False → 自動起動ON
+                EnableStartup();
+            }
+        }
+
+
+        void EnableStartup()
+        {
+            using (RegistryKey rk = Registry.CurrentUser.OpenSubKey(REG_PATH, true))
+            {
+                rk.SetValue(REG_NAME, Application.ExecutablePath);
+            }
+        }
+
+        void DisableStartup()
+        {
+            using (RegistryKey rk = Registry.CurrentUser.OpenSubKey(REG_PATH, true))
+            {
+                if (rk.GetValue(REG_NAME) != null)
+                {
+                    rk.DeleteValue(REG_NAME);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Hand.Play();
+            throw new Exception("Windows 11 End of Support.");
+            Application.Exit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string htmlPath = Path.Combine(Application.StartupPath, "11_eos.html");
+
+            if (!File.Exists(htmlPath))
+            {
+                // 無かったら静かに無視（仕様です）
+                return;
+            }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = htmlPath,
+                UseShellExecute = true
+            });
+        }
+    }
+}
